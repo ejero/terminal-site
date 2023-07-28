13c96af4df386476746602d0e39d2383683c626c
Rosita E <RositaEmakpo@Gmail.com>
2023-07-28 12:43:25 -0700

    Adds boilerplate

---
+++ script.js
@@ -0,0 +1,13 @@
+/* List avaiable commands */
+const helpCommands = [
+  'help       - Find out the commands you can run',
+
+  'about      - Learn all about me',
+
+  'portfolio  - See the list of projects I have made',
+
+  'contact    - See ways you can reach me...(no salker please)',
+
+  'clear      - Clear the screen'];
+
+console.log(helpCommands);
