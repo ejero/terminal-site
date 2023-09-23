aa50fc36e0ede3d4a4d979d3d766344fe69c2775
ejero <rositaemakpo@gmail.com>
2023-09-22 18:45:37 -0700

    Fix where previous command was showing up double

--- script.js
+++ script.js
@@ -1,78 +1,34 @@
 /* List avaiable commands when user enters 'help' */
 const helpCommands = [
-  'help       - Find out the commands you can run',
+  "help       - Find out the commands you can run",
 
-  'about      - Learn all about me',
+  "about      - Learn all about me",
 
-  'portfolio  - See the list of projects I have made',
+  "portfolio  - See the list of projects I have made",
 
-  'contact    - See ways you can reach me...(no salker please)',
+  "contact    - See ways you can reach me...(no salker please)",
 
-  'clear      - Clear the screen'
+  "clear      - Clear the screen",
 ];
 
-
 // For input field to be focused on page load
-const inputBox = document.getElementById('input');
+let inputBox = document.getElementById("input");
 
 //Inital focus on input field
-document.getElementById('input').focus();
+//document.getElementById('input').focus();
+inputBox.focus();
 
 //Re-foccus on inout field whenever it loses focus
-inputBox.addEventListener('blur', () => {
+inputBox.addEventListener("blur", () => {
   inputBox.focus();
 });
 
-
-// Listen for when the user hits the return/enter key
-inputBox.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
-    /* Get the valye from the input field. Trim any white space and convert to lowercase to better handling */
-    const inputValue = inputBox.value.trim().toLowerCase();
-
-
-    // Append the user command next to the prompt
-    inputBox.previousSibling.textContent += inputValue;
-
-
-
-    // // Display the command the user entered
-    // displayUserCommandEntered(inputValue);
-
-
-    // Check if user entered the 'help' command
-    if (inputValue === 'help') {
-      // run function to display the help commands
-      displayHelpCommands();
-    }
-
-    // Check if user entered the 'about' command
-    // Display about me info
-
-    // Check if user entered the 'portfolio' command
-    // Display portfolio info
-
-    // Check if user entered the 'contact' command
-    // Display contact info
-
-    // Check if user entered the 'clear' command
-    // Clear the screen
-
-
-    // Create a new input line for the user to enter another command
-    createNewInputLine();
-  }
-})
-
-
-
 // Function to display the help commands
 function displayHelpCommands() {
-
-  // Create a html <pre> element 
-  const preElement = document.createElement('pre');
+  // Create a html <pre> element
+  const preElement = document.createElement("pre");
   // Convert array to string with new line characters
-  preElement.textContent = helpCommands.join('\n');
+  preElement.textContent = helpCommands.join("\n");
 
   // Insert the <pre> element before the input field
   // inputBox.parentNode.insertBefore(preElement, inputBox);
@@ -80,25 +36,24 @@ function displayHelpCommands() {
   document.body.appendChild(preElement);
 }
 
-
-
-
 function createNewInputLine() {
   // Create the prompt, display user command, and the new input
-  const newPromptDiv = document.createElement('div');
+  const newPromptDiv = document.createElement("div");
 
-  const newPromptSpan = document.createElement('span');
-  newPromptSpan.className = 'prompt';
-  newPromptSpan.textContent = 'rosita@rosita.tech: ~ ';
+  const newPromptSpan = document.createElement("span");
+  newPromptSpan.className = "prompt";
+  newPromptSpan.textContent = "rosita@rosita.tech: ~ ";
 
-  const newInput = document.createElement('input');
-  newInput.type = 'text';
-  newInput.className = 'thick-caret-input';
-  newInput.name = 'name';
+  const newInput = document.createElement("input");
+  // Adding input type as URL to disable 1password plugin
+  newInput.type = "URL";
+  newInput.id = "input";
+  newInput.className = "thick-caret-input";
+  newInput.name = "name";
   newInput.required = true;
-  newInput.minlength = '4';
-  newInput.maxlength = '18';
-  newInput.size = '10';
+  newInput.minlength = "4";
+  newInput.maxlength = "18";
+  newInput.size = "10";
 
   newPromptDiv.appendChild(newPromptSpan);
   newPromptDiv.appendChild(newInput);
@@ -111,40 +66,43 @@ function createNewInputLine() {
   inputBox.focus();
 
   // Add the blur and keydown events to the new input
-  inputBox.addEventListener('blur', () => {
+  inputBox.addEventListener("blur", () => {
     inputBox.focus();
   });
 
-
-  inputBox.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
+  // Listen for when the user hits the return/enter key
+  inputBox.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
       const inputValue = inputBox.value.trim().toLowerCase();
 
-      // Append the user command next to the prompt
-      inputBox.previousSibling.appendChild(userCommand);
+      // Create a read-only text node with the command and append it to the current prompt
+      const commandText = document.createTextNode(inputValue);
+      inputBox.parentElement.insertBefore(commandText, inputBox);
+
+      // Remove the input box now that the command has been processed
+      inputBox.remove();
 
       // check if user entered 'help' command
-      if (inputValue === 'help') {
+      if (inputValue === "help") {
+        // run function to display the help commands
         displayHelpCommands();
       }
+
+      // Check if user entered the 'about' command
+      // Display about me info
+
+      // Check if user entered the 'portfolio' command
+      // Display portfolio info
+
+      // Check if user entered the 'contact' command
+      // Display contact info
+
+      // Check if user entered the 'clear' command
+      // Clear the screen
 
       // Creates a new line for command input
       createNewInputLine();
     }
   });
 }
-
-
-// // Display the command the user entered
-// function displayUserCommandEntered(command) {
-//   const commandEntered = document.createElement('p');
-//   commandEntered.textContent = `rosita@rosita:~$ ${command}`;
-//   document.body.appendChild(commandEntered);
-// }
-
-
-
-
-
-
-
+createNewInputLine();
